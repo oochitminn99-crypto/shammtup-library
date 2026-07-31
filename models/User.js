83d7d10d1f28f8a143fb677230953b2ed8057d9c
oochitminn99-crypto <oochitminn99@gmail.com>
2026-07-31 16:03:56 +0630

    add timestamp

--- models/User.js
+++ models/User.js
@@ -14,6 +14,6 @@ const UserSchema = new mongoose.Schema({
         required: true
     },
     
-})
+},{timestamps: true});
 
 export default mongoose.models.User || mongoose.model("User", UserSchema)
