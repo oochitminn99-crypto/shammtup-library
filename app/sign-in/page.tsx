195b7faadae7a121ae43f45c9a1acf639a4141bd
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-03 22:11:56 +0630

    redirect

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -19,7 +19,7 @@ export default function SignIn() {
         setPending(true);
 
         const res = await signIn("credentials", {
-            redirect: false,
+            redirect: true,
             email,
             password
         })
