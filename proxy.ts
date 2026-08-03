bbbc45cfda47a0f01bd7e580cdf7683ecbfe087f
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-03 22:29:38 +0630

    add session

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default function proxy(request: NextRequest) {
-    const token = request.cookies.get("session_token")
+    const token = request.cookies.get("session.user?.name")
 
     if(!token && request.nextUrl.pathname.startsWith("/dashboard")) {
         return NextResponse.redirect(new URL("/sign-in", request.url));
