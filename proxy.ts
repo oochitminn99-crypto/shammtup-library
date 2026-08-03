473e9852dc02ec822042a144cf5f67fe2ff0689d
oochitminn99-crypto <oochitminn99@gmail.com>
2026-08-03 21:46:39 +0630

    add proxy

---
+++ proxy.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function proxy(request: NextRequest) {
+    const auth = request.cookies.get("auth")
+
+    if(!auth && request.nextUrl.pathname.startsWith("/dashboard")) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/dashboard/:path*"],
+}
